cfaaa28a37a9fe3e476bdbeaf0a22ae2aa2a9ed6
deeptanshu20371 <88852018+deeptanshu20371@users.noreply.github.com>
2022-05-05 22:33:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs= require('fs');
+
+const hostname= '127.0.0.1';
+const port= 3000;
+const home=fs.readFileSync('index.html');
+const india=fs.readFileSync('india.html');
+const impact=fs.readFileSync('impact.html');
+
+const server= http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+} );
+
+server.listen(port,hostname,()=>{
+    console.log('Server is running at http://127.0.0.1:5500/');
+});
